8b13d7aefe011ad40357da229332f1508e8438e8
Jourdan Hamilton <jourdan.hamilton1@gmail.com>
2019-05-24 03:31:09 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(sounds = "Squawk")
-  puts "Sometimes a parrot will #{sounds}!"
+  puts "#{sounds}!"
 end
 end
